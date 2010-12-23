4f3f35ddddd292e8c321d1874e19a3ffa0baee7b
jasonolive <ottoramus@me.com>
2010-12-23 07:47:18 -0600

    Bump TOC for WoW Cataclysm (40000) Moved CRCLib into Core.lua Removed options temporarily to be integrated into AceConfig in future Replaced Ace2 Library with Ace3 Library (rev 998)

--- Core.lua
+++ Core.lua
@@ -1,180 +1,99 @@
-local L = AceLibrary("AceLocale-2.2"):new("QuestAnnouncer2");
+QuestAnnouncer4 = LibStub("AceAddon-3.0"):NewAddon("QuestAnnouncer4", "AceConsole-3.0", "AceComm-3.0", "AceEvent-3.0", "AceSerializer-3.0");
 
-local options = {
-	type = 'group',
-	args = {
-		PartyChat = {
-			type = 'toggle',
-			name = L["OPT_ANNOUNCE_NAME"],
-			desc = L["OPT_ANNOUNCE_DESC"],
-			get = "GetPartyChat",
-			set = "SetPartyChat",
-		},
-		History = {
-			type = 'toggle',
-			name = L["OPT_DISPLAY_NAME"],
-			desc = L["OPT_DISPLAY_DESC"],
-			get = "GetHistory",
-			set = "SetHistory",	
-		},
-	},
-};
+local questLog = {};
+local questLogUpdating = false;
 
--- Declare variables used throughout the addon
-	QuestAnnouncer2 = AceLibrary("AceAddon-2.0"):new("AceComm-2.0", "AceConsole-2.0", "AceEvent-2.0", "AceDB-2.0");
-	QuestAnnouncer2:RegisterChatCommand( {L["SLASHCMD_LONG"], L["SLASHCMD_SHORT"]}, options );
-	QuestAnnouncer2:RegisterDB( "QuestAnnouncer2DB", "QuestAnnouncer2DBPC" );
-	QuestAnnouncer2:SetCommPrefix(L["ADDON_PREFIX"]);
-	QuestAnnouncer2:RegisterDefaults( "profile", {PartyChat = false, History = false});
-
-	local QuestLog = {};
-	local QuestAnnouncer2_QuestLogUpdating = false;
-	local QuestAnnouncer2_ShowVersionWarning = true;
-	local QuestAnnouncer2_UpdateInterval = 15;
-	local COLOR_YELLOW = {r=1,g=1,b=0};
-	local COLOR_GREEN = {r=0,g=1,b=0};
-	local COLOR_RED = {r=1,g=0,b=0};
-
-function QuestAnnouncer2:OnEnable()
-	-- Register events and setup communications
-	self:Print(string.format(L["WELCOME_NOTE"], L["SLASHCMD_SHORT"]));
+function QuestAnnouncer4:OnInitialize()
 	self:RegisterEvent("QUEST_LOG_UPDATE");
 	self:RegisterEvent("UI_INFO_MESSAGE");
-	self:RegisterComm(L["ADDON_PREFIX"], "GROUP", "ReceiveMessage");
-	self:ScheduleRepeatingEvent(self.IntervalUpdate, QuestAnnouncer2_UpdateInterval, self);
+	self:RegisterComm("QA4");
+	self:Print("QuestAnnouncer 4 has successfully loaded.");
 end
 
-function QuestAnnouncer2:IntervalUpdate()
-	-- Send versioning information to others
-	if (GetNumPartyMembers()>0) then
-		self:SendCommMessage("GROUP", "VERSION", self.version, 0)
-	end
-end
-
-function QuestAnnouncer2:ReceiveMessage(prefix, sender, distribution, msgtype, arg1, arg2)
-	if (prefix == L["ADDON_PREFIX"]) and (msgtype ~= nil) and (distribution == "GROUP") and (sender ~= UnitName("player")) then
-		if msgtype == "ANNOUNCE" then
-			UIErrorsFrame:AddMessage(string.format(arg1[1], sender, arg1[2]),arg2.r,arg2.g,arg2.b,1.0,UIERRORS_HOLD_TIME)
-			if (self:GetHistory()) then
-				self:CustomPrint(arg2.r, arg2.g, arg2.b, nil, nil, nil, string.format(arg1[1], sender, arg1[2]))
-			end
-		elseif msgtype == "VERSION" then
-			if (tonumber(arg1) > tonumber(self.version)) and QuestAnnouncer2_ShowVersionWarning then
-				self:Print(string.format(L["OUT_OF_VERSION"], sender, arg1));
-				QuestAnnouncer2_ShowVersionWarning = false;
-			end
+function QuestAnnouncer4:OnCommReceived(prefix, message, distribution, sender)
+	if (prefix == "QA4") and (sender ~= UnitName("player")) then
+		local success, msg, color = self:Deserialize(message)
+		if (success) then
+			UIErrorsFrame:AddMessage(msg,color.r,color.g,color.b,1.0,UIERRORS_HOLD_TIME);
 		end
 	end
 end
 
-function QuestAnnouncer2:SendAnnouncement(message, color)
-	if (GetNumPartyMembers()>0) then
-		if (self:GetPartyChat()) then
-			SendChatMessage(string.format(message[1], UnitName("player"), message[2]),  "PARTY")
-		end
-		self:SendCommMessage("GROUP", "ANNOUNCE", message, color);
+function QuestAnnouncer4:ProcessAnnouncement(typ, msg)
+	local broadcast = '';
+	local color = {};
+	if (typ == 0) then
+		broadcast = string.format("%s\'s quest progress... %s", UnitName("player"), msg);
+		color = {r=1,g=1,b=0};
+	elseif (typ == 1) then
+		broadcast = string.format("%s has completed %s", UnitName("player"), msg);
+		color = {r=0,g=1,b=0};
+		UIErrorsFrame:AddMessage(string.format("You have completed %s", msg),color.r,color.g,color.b,1.0,UIERRORS_HOLD_TIME);
+	elseif (typ == -1) then
+		broadcast = string.format("%s has failed %s", UnitName("player"), msg);
+		color = {r=1,g=0,b=0};
+		UIErrorsFrame:AddMessage(string.format("You have failed %s", msg),color.r,color.g,color.b,1.0,UIERRORS_HOLD_TIME);
+	end
+	--if (GetNumPartyMembers()>0) then
+		local serializeddata = self:Serialize(broadcast, color);
+		self:SendCommMessage("QA4", serializeddata, "RAID");
+	--end
+end
+
+function QuestAnnouncer4:UI_INFO_MESSAGE(frame,msg)
+	if (msg ~= gsub(msg,"(.*):%s*([-%d]+)%s*/%s*([-%d]+)%s*$","%1",1)) then
+		self:ProcessAnnouncement(0, msg);
 	end
 end
 
-
-function QuestAnnouncer2:DisplayMyAnnouncements(message, color)
-	UIErrorsFrame:AddMessage(message,color.r, color.g, color.b,1.0,UIERRORS_HOLD_TIME);
-	if (self:GetHistory()) then
-		self:CustomPrint(color.r, color.g, color.b, nil, nil, nil, message)
-	end
-end
-
-function QuestAnnouncer2:UI_INFO_MESSAGE( message )
-	-- does the message fits our schema?
-	local questUpdateText = gsub(message,L["UI_SCHEMA"],"%1",1);
-	if (questUpdateText ~= message) then
-		if (self:GetHistory()) then
-			self:CustomPrint(COLOR_YELLOW.r, COLOR_YELLOW.g, COLOR_YELLOW.b, nil, nil, nil, string.format(L["DISPLAY_UI_MYUPDATE"], message))
-		end
-		self:SendAnnouncement({L["DISPLAY_UI_UPDATE"], message}, COLOR_YELLOW);
-	end
-end
-
-function QuestAnnouncer2:QUEST_LOG_UPDATE()
-	local new_QuestLog = {};
-	local questCRC = 0;
-	-- Check to see if thread is already running. If so, exit.
-	if ( QuestAnnouncer2_QuestLogUpdating ) then
+function QuestAnnouncer4:QUEST_LOG_UPDATE()
+	-- Check to see if thread is already running. If so, exit or lock if not.
+	if ( questLogUpdating ) then
 		return true;
 	end
-
-	-- Lock the thread
-	QuestAnnouncer2_QuestLogUpdating = true;
-
-	--Scan Quest Log
-	local QuestID=0;
-	while (GetQuestLogTitle(QuestID+1) ~= nil) do
-		QuestID = QuestID + 1;
+	questLogUpdating = true;
+	
+	local tmpQuestLog = {};
+	local QuestID=1;
+	while (GetQuestLogTitle(QuestID) ~= nil) do
 		local questTitle, _, _, _, isHeader, _, isComplete = GetQuestLogTitle(QuestID);
+		local questCRC = 0;
 		if (not isHeader) then
 			SelectQuestLogEntry(QuestID);
 
-			-- Get CRC value of current quest's description.  This allows for better tracking of quests with the same name.
-			local questDescription, questObjectives = GetQuestLogQuestText();
+			-- Get CRC32 value of current quest's description.  This allows for better tracking of quests with the same name.
+			local questDescription, _ = GetQuestLogQuestText();
 			if (questDescription) then
-				questCRC = CRCLib.crc(questDescription);
-
-				local status = 'active'
-				if isComplete == 1 then
-					status = 'complete'
-				elseif isComplete == -1 then
-					status = 'failed'
-				end
-				new_QuestLog[questCRC] = status
-
+				questCRC = self:CRC32(questDescription);
+				local status = 0
+				if (isComplete) then status = isComplete end;
+				tmpQuestLog[questCRC] = status;
 				-- Check for Quest changes
-				if not QuestLog[questCRC] or QuestLog[questCRC] ~= new_QuestLog[questCRC] then
-					if QuestLog[questCRC] == 'active' and new_QuestLog[questCRC] == 'complete' then
-						self:DisplayMyAnnouncements(string.format(L["DISPLAY_UI_MYCOMPLETE"], questTitle), COLOR_GREEN);
-						self:SendAnnouncement({L["DISPLAY_UI_COMPLETE"], questTitle}, COLOR_GREEN);
-					elseif (QuestLog[questCRC] == 'active' or QuestLog[questCRC] == 'complete') and new_QuestLog[questCRC] == 'failed' then
-						self:DisplayMyAnnouncements(string.format(L["DISPLAY_UI_MYFAILED"], questTitle), COLOR_RED);
-						self:SendAnnouncement({L["DISPLAY_UI_FAILED"], questTitle}, COLOR_RED);
+				if questLog[questCRC] and questLog[questCRC] ~= tmpQuestLog[questCRC] then
+					if questLog[questCRC] == 0 and tmpQuestLog[questCRC] == 1 then
+						self:ProcessAnnouncement(1, questTitle);
+					elseif (questLog[questCRC] == 0 or questLog[questCRC] == 1) and tmpQuestLog[questCRC] == -1 then
+						self:ProcessAnnouncement(-1, questTitle);						
 					end
 				end
-
-				QuestLog[questCRC] = nil
 			end
 		end
+		QuestID = QuestID + 1;
 	end
 
-	-- Switch the two tables, so that new_QuestLog becomes QuestLog
-	--new_QuestLog, QuestLog = QuestLog, new_QuestLog
-	--self.QuestLog = QuestLog
-	QuestLog = new_QuestLog;
+	-- Switch the two tables, so that tmpQuestLog becomes questLog
+	questLog = tmpQuestLog;
 	
 	-- Unlock the thread
-	QuestAnnouncer2_QuestLogUpdating = false;
-
+	questLogUpdating = false;
 end
 
-function QuestAnnouncer2:GetPartyChat()
-	return self.db.profile.PartyChat
-end
-
-function QuestAnnouncer2:SetPartyChat(name)
-self.db.profile.PartyChat = not self.db.profile.PartyChat
-	if self.db.profile.PartyChat then
-		self:Print(L["OPT_ANNOUNCE_ON"])
-	else
-		self:Print(L["OPT_ANNOUNCE_OFF"])
+local bit_band, bit_bxor, bit_rshift, str_byte, str_len = bit.band, bit.bxor, bit.rshift, string.byte, string.len
+local consts = { 0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3, 0x0EDB8832, 0x79DCB8A4,  0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91, 0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7, 0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5, 0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172, 0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B, 0x35B5A8FA, 0x42B2986C, 0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59, 0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F, 0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924, 0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D, 0x76DC4190, 0x01DB7106, 0x98D220BC, 0xEFD5102A, 0x71B18589, 0x06B6B51F, 0x9FBFE4A5, 0xE8B8D433, 0x7807C9A2, 0x0F00F934, 0x9609A88E, 0xE10E9818, 0x7F6A0DBB, 0x086D3D2D, 0x91646C97, 0xE6635C01, 0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E, 0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457, 0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65, 0x4DB26158, 0x3AB551CE, 0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB, 0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0, 0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9, 0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F, 0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81, 0xB7BD5C3B, 0xC0BA6CAD, 0xEDB88320, 0x9ABFB3B6, 0x03B6E20C, 0x74B1D29A, 0xEAD54739, 0x9DD277AF, 0x04DB2615, 0x73DC1683, 0xE3630B12, 0x94643B84, 0x0D6D6A3E, 0x7A6A5AA8, 0xE40ECF0B, 0x9309FF9D, 0x0A00AE27, 0x7D079EB1, 0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7, 0xFED41B76, 0x89D32BE0, 0x10DA7A5A, 0x67DD4ACC, 0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5, 0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B, 0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79, 0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236, 0xCC0C7795, 0xBB0B4703, 0x220216B9, 0x5505262F, 0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D, 0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x026D930A, 0x9C0906A9, 0xEB0E363F, 0x72076785, 0x05005713, 0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0x0CB61B38, 0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0x0BDBDF21, 0x86D3D2D4, 0xF1D4E242, 0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777, 0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45, 0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2, 0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB, 0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5, 0x47B2CF7F, 0x30B5FFE9, 0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF, 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D }
+function QuestAnnouncer4:CRC32(s)
+	local crc, l, i = 0xFFFFFFFF, str_len(s)
+	for i = 1, l, 1 do
+		crc = bit_bxor(bit_rshift(crc, 8), consts[bit_band(bit_bxor(crc, str_byte(s, i)), 0xFF) + 1])
 	end
-end
-
-function QuestAnnouncer2:GetHistory()
-	return self.db.profile.History
-end
-
-function QuestAnnouncer2:SetHistory(name)
-	self.db.profile.History = not self.db.profile.History
-	if self.db.profile.History then
-		self:Print(L["OPT_DISPLAY_ON"])
-	else
-		self:Print(L["OPT_DISPLAY_OFF"])
-	end
+	return bit_bxor(crc, -1)
 end
